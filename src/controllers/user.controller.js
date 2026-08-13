0af1a78175792c2d2069ea29a1bb2b18ddd4e5e0
Santosh Atediya <santoshatediya494@gmail.com>
2026-08-13 13:28:44 +0530

    add register controller and bugs fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,82 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+import {ApiError} from "../utils/ApiError.js";
+import {User, user} from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 const registerUser = asyncHandler( async(req, res) => {
- res.status(200).json({
+ /* res.status(200).json({
     message:"ok"
-  })
+
+
+  }) */
+
+// get user details from ffrontend
+//validation -  not empty
+// check if user already exits: username , email
+// check for images, check for avatar
+// upload them to cloudinary, avatar
+// create user object - create entry in db
+// remove password and refresh tokem field from response
+// check for user creation
+// return response
+
+const {fullName,email,username,password}  = req.body
+ console.log("email:",email);
+
+ if(
+  [fullName,email,password,username].some((field) => 
+  field?.trim()==="")
+ ){
+  throw new ApiError(400,"All field are required")
+ }
+const existedUser =user.findOne({
+  $or: [{username}, {email}]
 })
 
-export {registerUser}
+if(existedUser) {
+throw new ApiError(409,"User with email or username already exits")
+}
+
+const avatarLocalPath = req.files?.avatar[0]?.path;
+const  coverImageLocalPath = req.files?.coverImage[0]?.path
+
+if(!avatarLocalPath){
+  throw new ApiError(400,"Avatar file is required")
+} 
+
+const avatar = await uploadOnCloudinary(avatarLocalPath)
+const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+if(!avatar){
+   throw new ApiError(400,"Avatar file is required")
+}
+
+const user = await User.create({
+  fullName,
+  avatar:avatar.url,
+  coverImage:coverImage?.url || "",
+  email,
+  password,
+  username: username.toLowerCase()
+
+})
+
+ const createdUser = await User.findById(user._id).select(
+  "-password -refreshToken" //" - " mean nhi chahiye
+
+ )
+ if(!createdUser){
+  throw new ApiError(500,"Something went wrong while registering the user")
+ }
+
+ return res.status(201).json(
+  new ApiResponse(200, createdUser,"User registered Successfully")
+ )
+
+})
+
+
+
+
+
+
+export {registerUser};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,7 +50,7 @@ refreshToken:{
 
 userSchema.pre("save", async function (next) {  // pre middleware hooks hai
   if(!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password,10)
+  this.password = await bcrypt.hash(this.password,10)
   next()
 
 })
